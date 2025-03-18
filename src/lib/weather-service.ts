20b2631d59a4a2ce245a6f9f8cb9bd69dbae0451
Lucas Luann <75109074+LucasLuann@users.noreply.github.com>
2025-03-18 12:24:58 -0400

    feat: função de previsão do tempo para os próximos 7 dias

--- src/lib/weather-service.ts
+++ src/lib/weather-service.ts
@@ -3,8 +3,7 @@ import axios from "axios";
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-// Essa função recebe o nome da cidade e faz a requisição
-// para a API do clima, retornando os dados obtidos.
+// Essa função recebe o nome da cidade e faz a requisição do clima atual
 export const getWeather = async (city: string) => {
   try {
     const response = await axios.get(
@@ -16,3 +15,17 @@ export const getWeather = async (city: string) => {
     throw new Error("Erro ao buscar dados da API");
   }
 };
+
+// Essa função recebe o nome da cidade e faz a requisição das previsões
+export const getForecast = async (city: string) => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=7&lang=pt`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao buscar dados da API : ", error);
+    throw new Error("Erro ao buscar dados da API");
+  }
+};
+
