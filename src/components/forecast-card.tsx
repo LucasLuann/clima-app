3ed75d00ddd7e62507c51e7319ea7b6a95700ce5
Lucas Luann <75109074+LucasLuann@users.noreply.github.com>
2025-03-18 12:37:01 -0400

    feat: criando o componente de previsao

---
+++ src/components/forecast-card.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Weather } from "@/types/weather";
+import Image from "next/image";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+
+
+const ForecastCard = () => {
+  return (
+    <div>forecast-card</div>
+  )
+}
+
+export default ForecastCard
