7a8b4201b53095ec4103fc1e22786bc9341026b2
Lucas Luann <75109074+LucasLuann@users.noreply.github.com>
2025-03-18 12:37:20 -0400

    style: atualização de estilo no card de clima de hoje

--- src/components/weather-card.tsx
+++ src/components/weather-card.tsx
@@ -5,25 +5,28 @@ import { Weather } from "@/types/weather";
 
 const WeatherCard = ({ weather }: { weather: Weather }) => {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>
-          <div className="flex flex-col gap-1">
-            <p className="font-bold">{weather.location.name}</p>
-            <p className="text-sm">{weather.location.region}</p>
-            <p className="text-sm">{weather.location.country}</p>
-          </div>
+    <Card className="w-full p-4 bg-gradient-to-br from-blue-300 to-blue-700 text-white shadow-lg rounded-xl">
+      <CardHeader className="text-center ">
+        <CardTitle className="text-lg font-semibold tracking-wide">
+          Hoje
         </CardTitle>
       </CardHeader>
-      <CardContent>
+      <CardContent className="flex flex-col items-center gap-3">
+        <div className="text-center">
+          <p className="text-xl font-bold">{weather.location.name}</p>
+          <p className="text-sm opacity-80">
+            {weather.location.region}, {weather.location.country}
+          </p>
+        </div>
         <Image
           src={`https:${weather.current.condition.icon}`}
           alt={weather.current.condition.text}
-          width={100}
-          height={100}
+          width={80}
+          height={80}
+          className="drop-shadow-lg"
         />
-        <p className="font-bold">{weather.current.condition.text}</p>
-        <p className="text-2xl">{weather.current.temp_c}°C</p>
+        <p className="text-lg font-medium">{weather.current.condition.text}</p>
+        <p className="text-4xl font-bold">{weather.current.temp_c}°C</p>
       </CardContent>
     </Card>
   );
