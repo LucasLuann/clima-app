ad5512b846e0cd354014bb2b90ad1b68c5e29fc8
Lucas Luann <75109074+LucasLuann@users.noreply.github.com>
2025-03-19 12:22:48 -0400

    feat: inserindo previsao 3 dias

--- src/components/forecast-card.tsx
+++ src/components/forecast-card.tsx
@@ -1,42 +1,58 @@
-// import React from "react";
-// import { Forecast } from "@/types/forecast";
-// import Image from "next/image";
-// import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import React from "react";
+import { Forecast } from "@/types/forecast";
+import Image from "next/image";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Thermometer } from "lucide-react";
 
-// const ForecastCard = ({ forecast }: { forecast: Forecast }) => {
-//   return (
-//     <div className="flex gap-2 my-2">
-//       {forecast.forecast.forecastday.map((day) => (
-//         <Card
-//           key={day.date}
-//           className="w-full p-4 bg-gradient-to-br from-blue-300 to-blue-700 text-white shadow-lg rounded-xl"
-//         >
-//           <CardHeader className="text-center ">
-//             <CardTitle className="text-lg font-semibold tracking-wide">
-//               {new Date(day.date).toLocaleDateString("pt-BR")}
-//             </CardTitle>
-//           </CardHeader>
-//           <CardContent className="flex flex-col items-center gap-3">
-//             <div className="text-center">
-//               <p className="text-xl font-bold">{forecast.location.name}</p>
-//               <p className="text-sm opacity-80">
-//                 {forecast.location.region}, {forecast.location.country}
-//               </p>
-//             </div>
-//             <Image
-//               src={`https:${day.day.condition.icon}`}
-//               alt={day.day.condition.text}
-//               width={80}
-//               height={80}
-//               className="drop-shadow-lg"
-//             />
-//             <p className="text-lg font-medium">{day.day.condition.text}</p>
-//             <p className="text-4xl font-bold">{day.day.maxtemp_c}°C</p>
-//           </CardContent>
-//         </Card>
-//       ))}
-//     </div>
-//   );
-// };
+const ForecastCard = ({ forecast }: { forecast: Forecast }) => {
+  return (
+    <div className="flex gap-2 flex-wrap justify-between my-2 w-full">
+      {forecast?.forecast?.forecastday?.map((item: any) => (
+        <Card key={item.date} className="shadow-lg rounded-xl p-4 flex flex-1">
+          <CardHeader className="text-center ">
+            <CardTitle className="text-lg font-semibold tracking-wide">
+              {new Date(item.date).toLocaleString("pt-BR")}
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-col items-center gap-3">
+            <div className="text-center">
+              <p className="text-xl font-bold">{forecast.location.name}</p>
+              <p className="text-sm opacity-80">
+                {forecast.location.region}, {forecast.location.country}
+              </p>
+            </div>
+            <Image
+              src={`https:${item.day.condition.icon}`}
+              alt={item.day.condition.text}
+              width={80}
+              height={80}
+              className="drop-shadow-lg"
+            />
+            <p className="text-lg font-medium">{item.day.condition.text}</p>
+            <div className="flex justify-around w-full mt-5">
+              <div>
+                <div className="flex items-center">
+                  <Thermometer className="w-4 h-4" />
+                  <p className="text-center text-sm">Mínima</p>
+                </div>
+                <p className="text-3xl font-bold text-gray-500">{item.day.mintemp_c}°C</p>
+              </div>
+              <div>
+                <div className="flex items-center">
+                  <Thermometer className="w-4 h-4" />
+                  <p className="text-center text-sm ">Máxima</p>
+                </div>
+                <p className="text-3xl font-bold text-gray-500">
+                  {" "}
+                  {item.day.maxtemp_c}°C
+                </p>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
+};
 
-// export default ForecastCard;
+export default ForecastCard;
--- src/types/forecast.ts
+++ src/types/forecast.ts
@@ -1,4 +1,22 @@
 export interface Forecast {
+  location: {
+    name: string;
+    region: string;
+    country: string;
+    localtime: string;
+  };
+  current: {
+    temp_c: number;
+    condition: {
+      text: string;
+      icon: string;
+    };
+    wind_kph: number;
+    humidity: number;
+    feelslike_c: number;
+    wind_dir: string;
+    uv: number;
+  };
   forecast: {
     forecastday: {
       date: string;
