78dc402d827b0dcd7d84d0a86a5116058006bfde
Lucas Luann <75109074+LucasLuann@users.noreply.github.com>
2025-03-20 14:00:02 -0400

    fix: ajustando responsividade dos gráficos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -93,10 +93,12 @@ export default function Home() {
       {weather && (
         <div>
           <WeatherCard weather={weather} />
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-2">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-2">
             <ChartLine forecast={forecast} />
             <ChartLine forecast={forecast} />
-            <ChartLine forecast={forecast} />
+            <div className="col-span-1 sm:col-span-2 md:col-span-2 lg:col-span-1">
+              <ChartLine forecast={forecast} />
+            </div>
           </div>
           <ForecastCard forecast={forecast} />
         </div>
